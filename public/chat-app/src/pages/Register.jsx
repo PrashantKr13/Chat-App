9a227746b9f2e15d6bd2a13d2efd559fc2cc7e71
PrashantKr13 <prashant.kr1305@gmail.com>
2024-03-23 17:02:06 +0530

    Completed Register Page

--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -7,8 +7,10 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 import { registerRoute } from "../utils/APIRoutes";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+    const navigate = useNavigate();
     const [values, setValues] = useState({
         username: "",
         email: "",
@@ -29,7 +31,13 @@ export default function Register() {
             const { username, email, password, confirmpassword } = values;
             const { data } = await axios.post(registerRoute, {
                 username, email, password
-            })
+            });
+            if(data.status === false){
+                toast.error(data.msg, toastOptions);
+            }else{
+                localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+                navigate("/");
+            }
         }
     }
     const handleValidation = () => {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,24 @@
-module.exports.register = (req, res, next)=>{
-    console.log(req.body);
+const user = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next)=>{
+    try {
+        const {username, email, password} = req.body;
+    const usernameCheck = await user.findOne({username});
+    if(usernameCheck){
+        return res.json({msg: "Username already present.", status: false});
+    }
+    const emailCheck = await user.findOne({email});
+    if(emailCheck){
+        return res.json({msg: "Email already in use.", status: false});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await user.create({
+        username, email, password: hashedPassword
+    });
+    delete user.password;
+    return res.json({status: true, user});
+    } catch (error) {
+        next(error);
+    }
 }
